103b2d6d19f3b24178738f39ebe0d9e66965356a
AlgoriThai07 <thaiviet0703@gmail.com>
2026-05-16 21:39:17 -0500

    add categories

--- backend/models/item.ts
+++ backend/models/item.ts
@@ -1,9 +1,16 @@
 import type { firestore } from "firebase-admin";
 
+export type ItemCategory =
+  | "PPE"
+  | "LIFE_SUPPORT"
+  | "BLOOD"
+  | "MEDICATION"
+  | "GENERAL_SUPPLIES";
+
 export interface Item {
   id?: string;
   name: string;
   unit: string;
-  category: string;
+  category: ItemCategory;
   createdAt?: firestore.Timestamp;
 }
